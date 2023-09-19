32ec140f515d9f0f468be9b950b3435284c48283
Taz Lowry <travontazlowry@gmail.com>
2023-09-19 16:40:17 -0400

    add existing project

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HomeComponent } from "./home/home.component";
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "app-root",
+  template: `<main>
+    <header class="brand-name">
+      <img
+        class="brand-logo"
+        src="/assets/logo.svg"
+        alt="logo"
+        aria-hidden="true"
+      />
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
+  styleUrls: ["./app.component.css"],
+  imports: [HomeComponent],
+})
+export class AppComponent {
+  title = "homes";
+}
