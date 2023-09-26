3174c65e4be1f022cc44be83f75d0cc022955f1b
Taz Lowry <travontazlowry@gmail.com>
2023-09-26 16:05:20 -0400

    added css styling and added a ngFor loop to iterate over homes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { HomeComponent } from "./home/home.component";
 import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  standalone: true,
   selector: "app-root",
+  standalone: true,
+  imports: [HomeComponent],
   template: `<main>
     <header class="brand-name">
       <img
@@ -18,7 +19,6 @@ import { Component } from "@angular/core";
     </section>
   </main>`,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent],
 })
 export class AppComponent {
   title = "homes";
